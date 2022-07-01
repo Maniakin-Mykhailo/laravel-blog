8cac681567f012989f5d78b30bcbc26a517f5221
Cremson <89212136+Maniakin-Mykhailo@users.noreply.github.com>
2022-07-01 04:03:19 +0300

    table links and data output from the database

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class BlogController extends Controller
 {
-    //
+    public function index(){
+        $categories = Category::orderBy('title')->get();
+        $posts = Post::all();
+        return view('pages.index', [
+            'posts' => $posts,
+            'categories' => $categories,
+        ]);
+    }
+
+    public function getPostsByCategory($slug)
+    {
+        $categories = Category::orderBy('title')->get();
+        $current_category = Category::where('slug', $slug)->first();
+        return view('pages.index', [
+            'posts' => $current_category->posts,
+            'categories' => $categories,
+        ]);
+    }
 }
